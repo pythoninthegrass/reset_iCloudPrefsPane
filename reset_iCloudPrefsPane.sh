a9e15c7042ce40d49749cad5f1df622211a20af3
pythoninthegrass <lstephens@lance-mbp.corp.rhapsody.com>
2017-01-17 15:49:10 -0800

    Fix iCloud Preferences Pane Error
    Basic error handling to check for PLISTs / helpers then reset NVRAM for
    any lingering MobileMe tokens.

---
+++ reset_iCloudPrefsPane.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+
+# Exit upon failed command
+# set -e
+
+# Current user
+loggedInUser=$(ls -l /dev/console | cut -d " " -f 4)
+
+# Remove iCloud PLISTs
+# MobileMeAccounts.plist AKA iCloud Account PLIST
+cd /Users/$loggedInUser/Library/Preferences/
+if [ ! -f "MobileMeAccounts.plist" ]; then
+    sudo mv MobileMeAccounts.plist MobileMeAccounts.plist.old
+    echo "Removed MobileMeAccount.plist"
+else
+    echo "MobileMeAccount.plist not found."
+    # exit
+fi
+# com.apple.iCloudHelper
+cd /Users/$loggedInUser/Library/Caches/
+if [ ! -f "MobileMeAccounts.plist" ]; then
+    sudo mv com.apple.iCloudHelper com.apple.iCloudHelper.old
+    echo "Removed com.apple.iCloudHelper"
+else
+    echo "com.apple.iCloudHelper not found."
+    # exit
+fi
+
+# Reset NVRAM
+if [ $? -ne 0 ]; then
+    sudo /usr/sbin/nvram -c
+    echo "Reset NVRAM successfully."
+fi
+
+# Reboot
+if [ $? -ne 0 ]; then
+    echo "Restarting now. Hit CTRL-C to cancel."
+    sleep 5s
+    sudo reboot
+else
+    echo "No changes were made. Reboot cancelled."
+fi
+
+exit 0
